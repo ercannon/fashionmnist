0f74bfb7fb14c93b2fbd75d0403d7f695e4f7fa8
ercannon <87174255+ercannon@users.noreply.github.com>
2022-01-20 14:49:57 +0100

    anyadia primera carga

--- fashion/main.py
+++ fashion/main.py
@@ -5,3 +5,11 @@ Created on Thu Jan 20 14:47:07 2022
 
 @author: cannon
 """
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
