49424be11cd771c83cb70600da04ee81da515168
ercannon <87174255+ercannon@users.noreply.github.com>
2022-01-20 18:19:01 +0100

    entrenando modelo

--- fashion/main.py
+++ fashion/main.py
@@ -13,3 +13,22 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
 y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+"Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+#print(model.summary())
+#weights, biases = model.layers[1].get_weights()
+#print(weights)
+#print(biases)
+
+model.compile(loss="sparse_categorical_crossentropy",
+optimizer="sgd",
+metrics=["accuracy"])
+
+#history = model.fit(X_train, y_train, epochs=30, validation_data=(X_valid, y_valid))
